f6b03f5d735e022b8aef2e7630c3187b1f9eb725
Janne Huovari <janne.huovari@ptt.fi>
2020-11-06 14:28:14 +0200

    Other figures init

--- R/plots.R
+++ R/plots.R
@@ -9,6 +9,7 @@ prod_ind_plot <- function(data){
   ggplot(data = data, aes(time, lp_ind, colour = geo_name, size = geo_name == high_country)) +
     geom_line() +
     scale_size_manual(values = c(1.5,2.5), guide = "none") +
+    scale_color_discrete(palette = tula_pal) +
     guides(colour = guide_legend(reverse = TRUE)) +
     the_title_blank(c("x", "l")) +
     labs(y = glue("Indeksi, {base_year} = 100"))
---
+++ data-raw/get_oecd_ppp.R
@@ -0,0 +1,65 @@
+# OECD annual national account data
+
+library(OECD)
+library(tidyverse)
+library(countrycode)
+
+devtools::load_all()
+
+# dataset_list <- get_datasets()
+
+# dataset_list %>%
+#   filter(id %in% c("SNA_TABLE6A", "SNA_TABLE7A")) %>%
+#   knitr::kable()
+
+# dataset_list %>%
+#   filter(grepl("LFS", id)) %>%
+#   knitr::kable()
+#
+pdb_lv_str <- get_data_structure("PDB_LV")
+
+#
+pdb_lv_str$SUBJECT
+pdb_lv_str$MEASURE
+
+
+# Countries
+geo_levels <- countrycode(c("US", "CH", "DE", "DK", "ES", "FI", "FR", "IT", "NL", "SE"), "eurostat", "iso3c")
+
+
+
+# Measeures
+pdb_measures <-   c(
+  cp_ppp = "VPVOB"                # USD, constant prices, 2015 PPPs
+  )
+
+# Subjects
+pdb_subjects <-   c(
+  gdp_per_h = "T_GDPHRS"             # GDP per hour worked
+  )
+
+dat_oecd_levels0 <- get_dataset(dataset = "PDB_LV",
+                               filter = list(geo_leves, pdb_subjects, pdb_measures))
+
+
+
+
+dat_oecd_levels <- dat_oecd_levels0 %>%
+  transmute(
+    time = as.numeric(obsTime),
+    geo = as_factor(countrycode(LOCATION, "iso3c", "eurostat", nomatch = NULL)),
+    vars = fct_recode(SUBJECT, !!!pdb_subjects),
+    unit = fct_recode(MEASURE, !!!pdb_measures),
+    currency = as_factor(UNIT),
+    values = obsValue) %>%
+  mutate(vars = as_factor(vars)) %>%
+  spread(vars, values) %>%
+  filter(time >= 1970) %>%
+  droplevels() %>%
+  complete(time, geo) %>%
+  mutate(geo_name = as_factor(countrycode(geo, "eurostat", "cldr.name.fi", nomatch = NULL)))
+
+
+
+
+usethis::use_data(dat_oecd_levels, geo_levels, overwrite = TRUE)

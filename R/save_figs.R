73380a8bee36385dd9ca5f3692c8436f9ccb7ecd
Janne Huovari <janne.huovari@ptt.fi>
2021-11-26 15:36:16 +0200

    translations

--- R/save_figs.R
+++ R/save_figs.R
@@ -84,6 +84,45 @@ save_figs2 <- function(filename,
 
 }
 
+#' @describeIn save_fig
+#' @export
+#'
+#'
+save_figs2_en <- function(filename,
+                       plot = last_plot(),
+                       width = 13.5,
+                       height = 13.5,
+                       en = TRUE){
+  plot1 <- plot +
+    ggptt::the_title_blank(c("t", "l")) +
+    theme(plot.caption = element_blank())
+
+  # plot_slide <- plot +
+  #   theme(text = element_text(size=15))
+  #
+  # plot_slide_half <- plot +
+  #   ggptt::the_title_blank(c("l")) +
+  #   theme(text = element_text(size=11))
+  #
+  # ggplot2::ggsave(here::here("figures/pdf", paste0(filename, ".pdf")),
+  #                 plot = plot1,
+  #                 width = width, height = height, units = "cm")
+  #
+  # ggplot2::ggsave(here::here("figures/png", paste0(filename, ".png")),
+  #                 plot = plot1,  width = width, height = height, units = "cm")
+  # ggplot2::ggsave(here::here("figures", paste0(filename, "_esitys.png")),
+  #                 plot = plot_slide, width = 26, height = 12, units = "cm")
+  # ggplot2::ggsave(here::here("figures", paste0(filename, "_esitys_puolet.png")),
+  #                 plot = plot_slide_half, width = 16, height = 12, units = "cm")
+
+
+    ggplot2::ggsave(here::here("figures/pdf_en", paste0(filename, "_en.pdf")),
+                    plot1,
+                    width = width, height = height, units = "cm")
+
+
+}
+
 
 #' Save data from triplot to csv-files
 save_trip_plot_data <- function(filename,
--- data-raw/translation_data.R
+++ data-raw/translation_data.R
@@ -5,13 +5,16 @@ library(stringr)
 trans_source <-
   scan("data-raw/trans_source.csv", what = "character", sep = "\n") %>%
   str_split( "/") %>% unlist() %>%
-  str_replace("\\\\n" ,"\\\n")
+  str_replace("\\\\n" ,"\\\n") |>
+  # / in key are replaced with § to avoid split
+  str_replace("§", "/")
 
 
 trans_target <-
   scan("data-raw/trans_target.csv", what = "character", sep = "\n") %>%
   str_split( "/") %>% unlist()%>%
-  str_replace("\\\\n" ,"\\\n")
+  str_replace("\\\\n" ,"\\\n")|>
+  str_replace("§", "/")
 
 fig_translation <- purrr::set_names(trans_target, trans_source)
 

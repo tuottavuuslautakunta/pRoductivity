bfd6e54a20f21bcb59dc1e4f41d6cc7bb1ae06f1
Janne Huovari <janne.huovari@gmail.com>
2023-08-01 15:01:15 +0300

    tfp gmean first solve

--- data-raw/get_euklems_luiss_data.R
+++ data-raw/get_euklems_luiss_data.R
@@ -51,11 +51,45 @@ data_luiss_groups <- dat_klems_luiss |>
   gather(nace0, values, private, private_ex26, manu, manu_ex26, service) %>%
   mutate(nace0 = as_factor(nace0))
 
+## 	Growth Accounts Extended
+
+
+download.file("https://www.dropbox.com/s/d1cegmlamt0biy7/growth%20accounts.rds?dl=1", destfile = tmp, mode = "wb")
+
+dat_klems_luiss_ext0 <- readRDS(tmp)
+
+
+
+dat_klems_luiss_ext <-
+  dat_klems_luiss_ext0 |>
+  mutate(year = as.numeric(year)) |>
+  mutate(across(where(is.character), as_factor)) |>
+  rename(geo = geo_code, time = year, vars = var, values = value, nace_r2 = nace_r2_code)
+
+# data_luiss_groups_ext <- dat_klems_luiss_ext |>
+#   mutate(nace_r2 = forcats::fct_recode(nace_r2, TOTAL = "TOT")) |>
+#   filter(nace_r2 %in% main_nace_sna) |>
+#   # complete(geo, time, vars, nace_r2_code) |>
+#   group_by(geo, geo_name, time, vars) %>%
+#   summarise(private = sum(values[!(nace_r2 %in% c("TOTAL", "C26"))]),
+#             private_ex26 = private - values[nace_r2 == "C26"],
+#             manu = sum(values[nace_r2 == "C"]),
+#             manu_ex26 = manu - values[nace_r2 == "C26"],
+#             service = sum(values[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))])) %>%
+#   ungroup() %>%
+#   gather(nace0, values, private, private_ex26, manu, manu_ex26, service) %>%
+#   mutate(nace0 = as_factor(nace0))
+
+
 data_luiss_groups_tfp <-
-  dat_klems_luiss |>
-  filter(vars %in% c("VA_CP", "VATFP_I")) |>
+  bind_rows(
+    filter_recode(dat_klems_luiss,
+                  vars = c(VA_CP = "VA_CP")),
+    filter_recode(dat_klems_luiss_ext,
+                  vars = c(VATFP_I = "LP1TFP_I")))|>
   mutate(nace_r2 = forcats::fct_recode(nace_r2, TOTAL = "TOT")) |>
   filter(nace_r2 %in% main_nace_sna) |>
+  # filter(nace_r2 %in% c("B", "C", "D-E", "F", "G", "H", "I", "J", "K", "M", "N", "R", "S", "C26", "TOTAL")) |>
   # complete(geo, time, vars, nace_r2_code) |>
   spread(vars, values) |>
   group_by(geo, geo_name, time) |>
@@ -64,14 +98,18 @@ data_luiss_groups_tfp <-
             vacp_manu = sum(VA_CP[nace_r2 == "C"]),
             vacp_manu_ex26 = vacp_manu - VA_CP[nace_r2 == "C26"],
             vacp_service = sum(VA_CP[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))]),
-            tfp_private = weighted.mean(VATFP_I[!(nace_r2 %in% c("TOTAL", "C26"))], VA_CP[!(nace_r2 %in% c("TOTAL", "C26"))]),
-            tfp_private_ex26 = (tfp_private * vacp_private - VATFP_I[nace_r2 == "C26"] * VA_CP[nace_r2 == "C26"]) / (vacp_private - VA_CP[nace_r2 == "C26"]),
+            # Geometrinen keskiarvo painoina arvonlisä
+            tfp_private = ficomp::weighted_gmean(VATFP_I[!(nace_r2 %in% c("TOTAL", "C26"))], VA_CP[!(nace_r2 %in% c("TOTAL", "C26"))]),
+            # Geometrinen keskiarvo painoina arvonlisä, laskettu koko private ja c26 perustella. Private paino 1
+            tfp_private_ex26 = (tfp_private ^ 1 / VATFP_I[nace_r2 == "C26"] ^ (VA_CP[nace_r2 == "C26"]/vacp_private)) ^ (1 / (1 - VA_CP[nace_r2 == "C26"] / vacp_private)),
             tfp_manu = VATFP_I[nace_r2 == "C"],
-            tfp_manu_ex26 = (tfp_manu * vacp_manu - VATFP_I[nace_r2 == "C26"] * VA_CP[nace_r2 == "C26"]) / (vacp_manu - VA_CP[nace_r2 == "C26"]),
-            tfp_service = weighted.mean(VATFP_I[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))], VATFP_I[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))])) %>%
+            tfp_manu_ex26 = (tfp_manu ^ 1 / VATFP_I[nace_r2 == "C26"] ^ (VA_CP[nace_r2 == "C26"]/vacp_manu)) ^ (1 / (1 - VA_CP[nace_r2 == "C26"] / vacp_manu)),
+            tfp_service = ficomp::weighted_gmean(VATFP_I[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))], VATFP_I[nace_r2 %in% c(c("G", "H", "I", "J", "M", "N"))])) %>%
   ungroup() %>%
-  gather(nace0, values, -geo, - geo_name, -time) %>%
-  mutate(nace0 = as_factor(nace0))
+  gather(vars, values, -geo, - geo_name, -time) %>%
+  separate(vars, c("vars", "nace0"), sep = "_", extra = "merge") |>
+  mutate(nace0 = as_factor(nace0),
+         vars = as_factor(vars))
 
 ## Intangibles
 
@@ -168,6 +206,6 @@ data_luiss_intan_groups_main <-
   ungroup() %>%
   pivot_longer(cols = starts_with("ind_"), names_to = "ind", values_to = "values", names_prefix = "ind_")
 
-usethis::use_data(data_luiss_intan_groups_detail, data_luiss_groups, data_luiss_intan_groups_main,  overwrite = TRUE)
+usethis::use_data(data_luiss_intan_groups_detail, data_luiss_groups, data_luiss_intan_groups_main, data_luiss_groups_tfp,  overwrite = TRUE)
 
 data_luiss_intan_groups_main %>% write.csv2(file = "data_luiss_intan_groups_main.csv")
